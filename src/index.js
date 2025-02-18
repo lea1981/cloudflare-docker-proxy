38b0df10086eecab6fc09293d5797d2a74d2d9cc
lea1981 <96803272+lea1981@users.noreply.github.com>
2025-02-18 11:23:21 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -8,14 +8,14 @@ const CUSTOM_DOMAIN = "putaopi.top";
 
 const routes = {
   // production
-  ["docker." + CUSTOM_DOMAIN]: dockerHub,
-  ["quay." + CUSTOM_DOMAIN]: "https://quay.io",
-  ["gcr." + CUSTOM_DOMAIN]: "https://gcr.io",
-  ["k8s-gcr." + CUSTOM_DOMAIN]: "https://k8s.gcr.io",
-  ["k8s." + CUSTOM_DOMAIN]: "https://registry.k8s.io",
-  ["ghcr." + CUSTOM_DOMAIN]: "https://ghcr.io",
-  ["cloudsmith." + CUSTOM_DOMAIN]: "https://docker.cloudsmith.io",
-  ["ecr." + CUSTOM_DOMAIN]: "https://public.ecr.aws",
+  ["a." + CUSTOM_DOMAIN]: dockerHub,
+  //["quay." + CUSTOM_DOMAIN]: "https://quay.io",
+  //["gcr." + CUSTOM_DOMAIN]: "https://gcr.io",
+  //["k8s-gcr." + CUSTOM_DOMAIN]: "https://k8s.gcr.io",
+  //["k8s." + CUSTOM_DOMAIN]: "https://registry.k8s.io",
+  //["ghcr." + CUSTOM_DOMAIN]: "https://ghcr.io",
+  //["cloudsmith." + CUSTOM_DOMAIN]: "https://docker.cloudsmith.io",
+  //["ecr." + CUSTOM_DOMAIN]: "https://public.ecr.aws",
 
   // staging
   ["docker-staging." + CUSTOM_DOMAIN]: dockerHub,
